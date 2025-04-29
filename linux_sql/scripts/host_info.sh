9acb0b287b9ae73c9ce42acd5115a8cf969c497c
Curtis Lopes <c.lopes5@outlook.com>
2025-04-29 06:59:10 +0000

    Completed host_info.sh

---
+++ linux_sql/scripts/host_info.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Setup arguments
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+# Validate arguments
+if [ "$#" -ne 5 ]; then
+  echo "Illegal number of parameters"
+  exit 1
+fi
+
+# Parse host hardware specs and assign output to variables
+hostname=$(hostname -f)
+lscpu_out=`lscpu`
+cpu_number=$(echo "$lscpu_out" | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_architecture=$(echo "$lscpu_out" | egrep "^\s*Architecture:" | awk '{print $2}' | xargs)
+cpu_model=$(echo "$lscpu_out" | egrep "^\s*Model name:" | cut -d: -f2 | xargs)
+cpu_mhz=$(grep "cpu MHz" /proc/cpuinfo | head -n1 | awk '{print $4}' | xargs)
+l2_cache=$(echo "$lscpu_out" | egrep "L2 cache:" | awk '{print $3}' | xargs)
+total_mem=$(vmstat --unit M | tail -1 | awk '{print $4}')
+timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+
+#INSERT statement from specification variables
+insert_statement="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, timestamp, total_mem)
+VALUES ('$hostname', $cpu_number, '$cpu_architecture', '$cpu_model', $cpu_mhz, '$l2_cache', '$timestamp', $total_mem);"
+
+#Execute INSERT statement
+export PGPASSWORD=$psql_password
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_statement"
+
+exit $?
