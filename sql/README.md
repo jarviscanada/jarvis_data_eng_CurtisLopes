bfbc05c3e8ec566f9f57339f59d9db379b473988
Curtis Lopes <c.lopes5@outlook.com>
2025-05-09 07:53:05 +0000

    Updating README, formatting queries.sql

--- sql/README.md
+++ sql/README.md
@@ -40,13 +40,355 @@ CREATE TABLE cd.bookings (
 	CONSTRAINT fk_bookings_memid FOREIGN KEY (memid) REFERENCES cd.members(memid)
 );
 ```
-###### Question 1: Show all members 
+###### Question 1: Insert some data into a table
 ```sql
-SELECT *
-FROM cd.members
+INSERT INTO cd.facilities
+    (facid, name, membercost, guestcost, initialoutlay, monthlymaintenance)
+    VALUES (9, 'Spa', 20, 30, 100000, 800);
 ```
-###### Question 2: Lorem ipsum...
+###### Question 2: Insert calculated data into a table
 ```sql
+INSERT INTO cd.facilities (
+    facid, name, membercost, guestcost,
+    initialoutlay, monthlymaintenance
+)
+SELECT
+    (
+    SELECT
+        MAX(facid)
+    FROM
+        cd.facilities
+    )+ 1,
+    'Spa',
+    20,
+    30,
+    100000,
+    800;
+```
+
+###### Question 3: Update some existing data
+```sql
+UPDATE cd.facilities
+    SET initialoutlay = 10000
+    WHERE facid = 1;
+```
+###### Question 4: Update a row based on the contents of another row
+```sql
+UPDATE cd.facilities facs
+SET
+    membercost = (
+        SELECT membercost * 1.1
+        FROM cd.facilities
+        WHERE facid = 0
+    ),
+    guestcost = (
+        SELECT guestcost * 1.1
+        FROM cd.facilities
+        WHERE facid = 0
+    )
+WHERE
+    facs.facid = 1;
+```
+###### Question 5: Delete all bookings
+```sql
+DELETE FROM cd.bookings;
+```
+###### Question 6: Delete a member from the cd.members table
+```sql
+DELETE FROM
+    cd.members
+WHERE
+    memid = 37;
+```
+###### Question 7: Control which rows are retrieved - part 2
+```sql
+SELECT
+    facid,
+    name,
+    membercost,
+    monthlymaintenance
+FROM
+    cd.facilities
+WHERE
+    membercost > 0
+  AND (
+    membercost < monthlymaintenance / 50.0
+  );
+```
+###### Question 8: Basic string searches
+```sql
+SELECT
+    *
+FROM
+    cd.facilities
+WHERE
+    name LIKE '%Tennis%';
+```
+###### Question 9: Matching against multiple possible values
+```sql
+SELECT
+    *
+FROM
+    cd.facilities
+WHERE
+    facid in (1, 5);
+```
+###### Question 10: Working with dates
+```sql
+SELECT
+    memid,
+    surname,
+    firstname,
+    joindate
+FROM
+    cd.members
+WHERE
+    joindate >= '2012-09-01';
+```
+###### Question 11: Combining results from multiple queries
+```sql
+SELECT
+    surname
+FROM
+    cd.members
+UNION
+SELECT
+    name
+FROM
+    cd.facilities;
+```
+###### Question 12: Retrieve the start times of members' bookings
+```sql
+SELECT
+    bks.starttime
+FROM
+    cd.bookings bks
+    INNER JOIN cd.members mems ON mems.memid = bks.memid
+WHERE
+    mems.firstname = 'David'
+    AND mems.surname = 'Farrell';
+```
+###### Question 13: Work out the start times of bookings for tennis courts
+```sql
+SELECT
+    bks.starttime AS start,
+    facs.name AS name
+FROM
+    cd.facilities facs
+    INNER JOIN cd.bookings bks ON facs.facid = bks.facid
+WHERE
+    facs.name in (
+        'Tennis Court 2', 'Tennis Court 1'
+    )
+    AND bks.starttime >= '2012-09-21'
+    AND bks.starttime < '2012-09-22'
+ORDER BY
+    bks.starttime;
+```
+###### Question 14: Produce a list of all members, along with their recommender
+```sql
+SELECT
+    mems.firstname AS memfname,
+    mems.surname AS memsname,
+    recs.firstname AS recfname,
+    recs.surname AS recsname
+FROM
+    cd.members mems
+    LEFT OUTER JOIN cd.members recs ON recs.memid = mems.recommendedby
+ORDER BY
+    memsname,
+    memfname;
+```
+###### Question 15: Produce a list of all members who have recommended another member
+```sql
+SELECT
+    DISTINCT recs.firstname AS firstname,
+    recs.surname AS surname
+FROM
+    cd.members mems
+    INNER JOIN cd.members recs ON recs.memid = mems.recommendedby
+ORDER BY
+    surname,
+    firstname;
+```
+###### Question 16: Produce a list of all members, along with their recommender, using no joins.
+```sql
+SELECT
+    DISTINCT mems.firstname || ' ' || mems.surname AS member,
+    (
+        SELECT
+            recs.firstname || ' ' || recs.surname AS recommender
+        FROM
+            cd.members recs
+        WHERE
+            recs.memid = mems.recommendedby
+    )
+FROM
+    cd.members mems
+ORDER BY
+    member;
+```
+###### Question 17: Count the number of recommendations each member makes.
+```sql
+SELECT recommendedby, COUNT(*)
+	FROM cd.members
+	WHERE recommendedby IS NOT NULL
+	GROUP BY recommendedby
+ORDER BY recommendedby;
+```
+###### Question 18: List the total slots booked per facility
+```sql
+SELECT
+    facid,
+    SUM(slots) AS "Total Slots"
+FROM
+    cd.bookings
+GROUP BY
+    facid
+ORDER BY
+    facid;
+```
+###### Question 19: List the total slots booked per facility in a given month
+```sql
+SELECT
+    facid,
+    SUM(slots) AS "Total Slots"
+FROM
+    cd.bookings
+WHERE
+    starttime >= '2012-09-01'
+    AND starttime < '2012-10-01'
+GROUP BY
+    facid
+ORDER BY
+    SUM(slots);
+```
+###### Question 20: List the total slots booked per facility per month
+```sql
+SELECT
+    facid,
+    EXTRACT(
+        MONTH
+        FROM
+            starttime
+    ) AS MONTH,
+    SUM(slots) AS "Total Slots"
+FROM
+  cd.bookings
+WHERE
+    EXTRACT(
+        YEAR
+        FROM
+        starttime
+    ) = 2012
+GROUP BY
+    facid,
+    MONTH
+ORDER BY
+    facid,
+    MONTH;
+```
+###### Question 21: Find the count of members who have made at least one booking
+```sql
+SELECT
+    COUNT(DISTINCT memid)
+FROM
+    cd.bookings;
+```
+###### Question 22: List each member's first booking after September 1st 2012
+```sql
+SELECT
+    mems.surname,
+    mems.firstname,
+    mems.memid,
+    MIN(bks.starttime) AS starttime
+FROM
+    cd.bookings bks
+        INNER JOIN cd.members mems ON mems.memid = bks.memid
+WHERE
+    starttime >= '2012-09-01'
+GROUP BY
+    mems.surname,
+    mems.firstname,
+    mems.memid
+ORDER BY
+    mems.memid;
+```
+###### Question 23: Produce a list of member names, with each row containing the total member count
+```sql
+SELECT
+    COUNT(*) over(),
+    firstname,
+    surname
+FROM
+    cd.members
+ORDER BY
+    joindate;
+```
+###### Question 24: Produce a numbered list of members
+```sql
+SELECT
+    ROW_NUMBER() over(
+    ORDER BY
+      joindate
+  ),
+    firstname,
+    surname
+FROM
+    cd.members
+ORDER BY
+    joindate;
+```
+###### Question 25: Output the facility id that has the highest number of slots booked, again
+```sql
+SELECT
+    facid,
+    total
+FROM
+    (
+        SELECT
+            facid,
+            SUM(slots) total,
+            RANK() over (
+        ORDER BY
+          sum(slots) desc
+      ) rank
+        FROM
+            cd.bookings
+        GROUP BY
+            facid
+    ) AS ranked
+WHERE
+    rank = 1;
+```
+###### Question 26: Format the names of members
+```sql
+SELECT
+    surname || ', ' || firstname AS name
+FROM
+    cd.members;
+```
+###### Question 27: Find telephone numbers with parentheses
+```sql
+SELECT
+    memid,
+    telephone
+FROM
+    cd.members
+WHERE
+    telephone ~ '[()]';
+```
+###### Question 28: Count the number of members whose surname starts with each letter of the alphabet
+```sql
+SELECT
+    SUBSTRING(mems.surname, 1, 1) AS letter,
+    COUNT(*) AS COUNT
+FROM
+    cd.members mems
+GROUP BY
+    letter
+ORDER BY
+    letter;
 ```
 
 
--- sql/queries.sql
+++ sql/queries.sql
@@ -1,310 +1,321 @@
---Question 1:
+--Question 1: Insert some data into a table
 INSERT INTO cd.facilities
     (facid, name, membercost, guestcost, initialoutlay, monthlymaintenance)
-    values (9, 'Spa', 20, 30, 100000, 800);
+    VALUES (9, 'Spa', 20, 30, 100000, 800);
 
---Question 2:
+--Question 2: Insert calculated data into a table
 INSERT INTO cd.facilities (
     facid, name, membercost, guestcost,
     initialoutlay, monthlymaintenance
 )
-values
-    (9, 'Spa', 20, 30, 100000, 800);
+SELECT
+    (
+        SELECT
+            MAX(facid)
+        FROM
+            cd.facilities
+    )+ 1,
+    'Spa',
+    20,
+    30,
+    100000,
+    800;
 
---Question 3:
+
+--Question 3: Update some existing data
 UPDATE cd.facilities
     SET initialoutlay = 10000
     WHERE facid = 1;
 
---Question 4:
+--Question 4: Update a row based on the contents of another row
 UPDATE cd.facilities facs
 SET
     membercost = (
-        select membercost * 1.1
-        from cd.facilities
-        where facid = 0
+        SELECT membercost * 1.1
+        FROM cd.facilities
+        WHERE facid = 0
     ),
     guestcost = (
-        select guestcost * 1.1
-        from cd.facilities
-        where facid = 0
+        SELECT guestcost * 1.1
+        FROM cd.facilities
+        WHERE facid = 0
     )
 WHERE
     facs.facid = 1;
 
---Question 5:
+--Question 5: Delete all bookings
 DELETE FROM cd.bookings;
 
---Question 6:
+--Question 6: Delete a member from the cd.members table
 DELETE FROM
-  cd.members
+    cd.members
 WHERE
-  memid = 37;
+    memid = 37;
 
---Question 7:
-select
-  facid,
-  name,
-  membercost,
-  monthlymaintenance
-from
-  cd.facilities
-where
-  membercost > 0
-  and (
+--Question 7: Control which rows are retrieved - part 2
+SELECT
+    facid,
+    name,
+    membercost,
+    monthlymaintenance
+FROM
+    cd.facilities
+WHERE
+    membercost > 0
+  AND (
     membercost < monthlymaintenance / 50.0
   );
 
---Question 8:
-select
-  *
-from
-  cd.facilities
-where
-  name like '%Tennis%'
+--Question 8: Basic string searches
+SELECT
+    *
+FROM
+    cd.facilities
+WHERE
+    name LIKE '%Tennis%';
 
---Question 9:
-select
-  *
-from
-  cd.facilities
-where
-  facid in (1, 5);
+--Question 9: Matching against multiple possible values
+SELECT
+    *
+FROM
+    cd.facilities
+WHERE
+    facid in (1, 5);
 
---Question 10:
-select
-  memid,
-  surname,
-  firstname,
-  joindate
-from
-  cd.members
-where
-  joindate >= '2012-09-01';
+--Question 10: Working with dates
+SELECT
+    memid,
+    surname,
+    firstname,
+    joindate
+FROM
+    cd.members
+WHERE
+    joindate >= '2012-09-01';
 
---Question 11:
-select
-  surname
-from
-  cd.members
-union
-select
-  name
-from
-  cd.facilities;
+--Question 11: Combining results from multiple queries
+SELECT
+    surname
+FROM
+    cd.members
+UNION
+SELECT
+    name
+FROM
+    cd.facilities;
 
---Question 12:
-select
-  bks.starttime
-from
-  cd.bookings bks
-  inner join cd.members mems on mems.memid = bks.memid
-where
-  mems.firstname = 'David'
-  and mems.surname = 'Farrell';
+--Question 12: Retrieve the start times of members' bookings
+SELECT
+    bks.starttime
+FROM
+    cd.bookings bks
+    INNER JOIN cd.members mems ON mems.memid = bks.memid
+WHERE
+    mems.firstname = 'David'
+    AND mems.surname = 'Farrell';
 
---Question 13:
-select
-  bks.starttime as start,
-  facs.name as name
-from
-  cd.facilities facs
-  inner join cd.bookings bks on facs.facid = bks.facid
-where
-  facs.name in (
-    'Tennis Court 2', 'Tennis Court 1'
-  )
-  and bks.starttime >= '2012-09-21'
-  and bks.starttime < '2012-09-22'
-order by
-  bks.starttime;
+--Question 13: Work out the start times of bookings for tennis courts
+SELECT
+    bks.starttime AS start,
+    facs.name AS name
+FROM
+    cd.facilities facs
+    INNER JOIN cd.bookings bks ON facs.facid = bks.facid
+WHERE
+    facs.name in (
+        'Tennis Court 2', 'Tennis Court 1'
+    )
+    AND bks.starttime >= '2012-09-21'
+    AND bks.starttime < '2012-09-22'
+ORDER BY
+    bks.starttime;
 
---Question 14:
-select
-  mems.firstname as memfname,
-  mems.surname as memsname,
-  recs.firstname as recfname,
-  recs.surname as recsname
-from
-  cd.members mems
-  left outer join cd.members recs on recs.memid = mems.recommendedby
-order by
-  memsname,
-  memfname;
+--Question 14: Produce a list of all members, along with their recommender
+SELECT
+    mems.firstname AS memfname,
+    mems.surname AS memsname,
+    recs.firstname AS recfname,
+    recs.surname AS recsname
+FROM
+    cd.members mems
+    LEFT OUTER JOIN cd.members recs ON recs.memid = mems.recommendedby
+ORDER BY
+    memsname,
+    memfname;
 
---Question 15:
-select
-  distinct recs.firstname as firstname,
-  recs.surname as surname
-from
-  cd.members mems
-  inner join cd.members recs on recs.memid = mems.recommendedby
-order by
-  surname,
-  firstname;
+--Question 15: Produce a list of all members who have recommended another member
+SELECT
+    DISTINCT recs.firstname AS firstname,
+    recs.surname AS surname
+FROM
+    cd.members mems
+    INNER JOIN cd.members recs ON recs.memid = mems.recommendedby
+ORDER BY
+    surname,
+    firstname;
 
---Question 16:
-select
-  distinct mems.firstname || ' ' || mems.surname as member,
-  (
-    select
-      recs.firstname || ' ' || recs.surname as recommender
-    from
-      cd.members recs
-    where
-      recs.memid = mems.recommendedby
-  )
-from
-  cd.members mems
-order by
-  member;
+--Question 16: Produce a list of all members, along with their recommender, using no joins.
+SELECT
+    DISTINCT mems.firstname || ' ' || mems.surname AS member,
+    (
+        SELECT
+            recs.firstname || ' ' || recs.surname AS recommender
+        FROM
+            cd.members recs
+        WHERE
+            recs.memid = mems.recommendedby
+    )
+FROM
+    cd.members mems
+ORDER BY
+    member;
 
---Question 17:
-select recommendedby, count(*)
-	from cd.members
-	where recommendedby is not null
-	group by recommendedby
-order by recommendedby;
+--Question 17: Count the number of recommendations each member makes.
+SELECT recommendedby, COUNT(*)
+	FROM cd.members
+	WHERE recommendedby IS NOT NULL
+	GROUP BY recommendedby
+ORDER BY recommendedby;
 
---Question 18:
-select
-  facid,
-  sum(slots) as "Total Slots"
-from
-  cd.bookings
-group by
-  facid
-order by
-  facid;
-
---Question 19:
-select
+--Question 18: List the total slots booked per facility
+SELECT
     facid,
-    sum(slots) as "Total Slots"
-from
+    SUM(slots) AS "Total Slots"
+FROM
     cd.bookings
-where
-    starttime >= '2012-09-01'
-    and starttime < '2012-10-01'
-group by
+GROUP BY
     facid
-order by
-    sum(slots);
+ORDER BY
+    facid;
 
---Question 20:
-select
-  facid,
-  extract(
-    month
-    from
-      starttime
-  ) as month,
-  sum(slots) as "Total Slots"
-from
+--Question 19: List the total slots booked per facility in a given month
+SELECT
+    facid,
+    SUM(slots) AS "Total Slots"
+FROM
+    cd.bookings
+WHERE
+    starttime >= '2012-09-01'
+    AND starttime < '2012-10-01'
+GROUP BY
+    facid
+ORDER BY
+    SUM(slots);
+
+--Question 20: List the total slots booked per facility per month
+SELECT
+    facid,
+    EXTRACT(
+        MONTH
+        FROM
+            starttime
+    ) AS MONTH,
+    SUM(slots) AS "Total Slots"
+FROM
   cd.bookings
-where
-  extract(
-    year
-    from
-      starttime
-  ) = 2012
-group by
-  facid,
-  month
-order by
-  facid,
-  month;
+WHERE
+    EXTRACT(
+        YEAR
+        FROM
+        starttime
+    ) = 2012
+GROUP BY
+    facid,
+    MONTH
+ORDER BY
+    facid,
+    MONTH;
 
---Question 21:
-select
-    count(distinct memid)
-from
+--Question 21: Find the count of members who have made at least one booking
+SELECT
+    COUNT(DISTINCT memid)
+FROM
     cd.bookings;
 
---Question 22:
-select
+--Question 22: List each member's first booking after September 1st 2012
+SELECT
     mems.surname,
     mems.firstname,
     mems.memid,
-    min(bks.starttime) as starttime
-from
+    MIN(bks.starttime) AS starttime
+FROM
     cd.bookings bks
-        inner join cd.members mems on mems.memid = bks.memid
-where
+        INNER JOIN cd.members mems ON mems.memid = bks.memid
+WHERE
     starttime >= '2012-09-01'
-group by
+GROUP BY
     mems.surname,
     mems.firstname,
     mems.memid
-order by
+ORDER BY
     mems.memid;
 
---Question 23:
-select
-    count(*) over(),
+--Question 23: Produce a list of member names, with each row containing the total member count
+SELECT
+    COUNT(*) over(),
     firstname,
     surname
-from
+FROM
     cd.members
-order by
+ORDER BY
     joindate;
 
---Question 24:
-select
-    row_number() over(
-    order by
+--Question 24: Produce a numbered list of members
+SELECT
+    ROW_NUMBER() over(
+    ORDER BY
       joindate
   ),
     firstname,
     surname
-from
+FROM
     cd.members
-order by
+ORDER BY
     joindate;
 
---Question 25:
-select
+--Question 25: Output the facility id that has the highest number of slots booked, again
+SELECT
     facid,
     total
-from
+FROM
     (
-        select
+        SELECT
             facid,
-            sum(slots) total,
-            rank() over (
-        order by
+            SUM(slots) total,
+            RANK() over (
+        ORDER BY
           sum(slots) desc
       ) rank
-        from
+        FROM
             cd.bookings
-        group by
+        GROUP BY
             facid
-    ) as ranked
-where
+    ) AS ranked
+WHERE
     rank = 1;
 
---Question 26:
-select
-    surname || ', ' || firstname as name
-from
+--Question 26: Format the names of members
+SELECT
+    surname || ', ' || firstname AS name
+FROM
     cd.members;
 
---Question 27:
-select
+--Question 27: Find telephone numbers with parentheses
+SELECT
     memid,
     telephone
-from
+FROM
     cd.members
-where
+WHERE
     telephone ~ '[()]';
 
---Question 28:
-select
-    substr (mems.surname, 1, 1) as letter,
-    count(*) as count
-from
+--Question 28: Count the number of members whose surname starts with each letter of the alphabet
+SELECT
+    SUBSTRING(mems.surname, 1, 1) AS letter,
+    COUNT(*) AS COUNT
+FROM
     cd.members mems
-group by
+GROUP BY
     letter
-order by
+ORDER BY
     letter;
