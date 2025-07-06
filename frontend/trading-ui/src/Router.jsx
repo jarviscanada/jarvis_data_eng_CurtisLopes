dc4f1e6887ec983ac8a939bda1622edf185df881
Curtis Lopes <c.lopes5@outlook.com>
2025-07-06 00:43:37 -0400

    Updating Dashboard and NavBar files

---
+++ frontend/trading-ui/src/Router.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Dashboard from './page/Dashboard/Dashboard';
+
+// Initialize Router Component
+export default function Router() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/" element={<Dashboard />} />
+                <Route exact path="/dashboard" element={<Dashboard />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
