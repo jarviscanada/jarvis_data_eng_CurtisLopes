151873b2f70859c92cf7949cb4fed1f5cc1b5fee
Curtis Lopes <c.lopes5@outlook.com>
2025-08-01 13:47:21 -0400

    Updating script, removing sample input from tables

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -26,8 +26,7 @@ timestamp=$(date "+%Y-%m-%d %H:%M:%S")
 
 #INSERT statement from specification variables
 insert_statement="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, timestamp, total_mem)
-VALUES ('$hostname', $cpu_number, '$cpu_architecture', '$cpu_model', $cpu_mhz, '$l2_cache', '$timestamp', $total_mem)
-ON CONFLICT (hostname) DO NOTHING;"
+VALUES ('$hostname', $cpu_number, '$cpu_architecture', '$cpu_model', $cpu_mhz, '$l2_cache', '$timestamp', $total_mem);"
 
 #Execute INSERT statement
 export PGPASSWORD=$psql_password
